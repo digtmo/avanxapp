169b2463282c44296dfe5ff3fa272764809dc1c5
David Torres <davidtorresim@gmail.com>
2022-11-15 21:53:57 -0300

    Api2

--- src/Components/Api.js
+++ src/Components/Api.js
@@ -4,14 +4,13 @@ import axios from 'axios'
 
 const endpoint = "/api/discussion/v1/threads/"
 const curso = "course-v1:AIEP+GPTI+C9GPTI"
-const email = "david.torres@avanxa.com"
-const password = "Tricahueb11$"
+/* const email = "david.torres@avanxa.com"
+const password = "Tricahueb11$" */
 const raiz = "https://elearning.avanxa.com"
-const authe = '/user_api/v1/account/login_session/'
+/* const authe = '/user_api/v1/account/login_session/' */
 
 
 const GetToken = () => {
-    /* useEffect(() => { */
         axios({
             method:'get',
             url: raiz+'/csrf/api/v1/token', 
@@ -20,22 +19,25 @@ const GetToken = () => {
         if (response.status === 200){
             const token = response.data
             console.log("OK GetToken",token)
+            token2.push(token)
         }})
         .catch(function(error){
             console.log("Error Const GetToken:", error)
         }) 
-   /*  })   */ 
+    const token2 = []
+    console.log("token2",token2)
 }
 
 GetToken();
-
-
 
 const auth = () => {
     axios({
         method:'post',
         url:'https://elearning.avanxa.com/user_api/v1/account/login_session',
-        
+       headers:{
+            'Referer': raiz,
+            'x-csrftoken' : "crsfToken"  
+        }, 
         params:{        
             email: 'david.torres@avanxa.com',
             password: 'Tricahueb11$'
@@ -44,14 +46,19 @@ const auth = () => {
     .then(function (response) {
         if (response.status === 200){
             const data = response.data
-            console.log("Const auth",data)
+            console.log("OK Const auth",data)
         }})
     .catch(function(error){
         console.log("Error auth", error)
         }) 
 }
 
-auth();
+const prueba = () => {
+    setTimeout(auth,5000)
+}
+
+prueba();
+
 
 
 const Api = () => {
